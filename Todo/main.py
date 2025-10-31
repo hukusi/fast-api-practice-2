24455fb0872fee2e2b0f50b120ce4e2900046cbb
hukusi <hukushi.sakura@gmail.com>
2025-11-01 01:51:04 +0900

    一旦アップ

---
+++ Todo/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import models
+from database import engine, SessionLocal
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
