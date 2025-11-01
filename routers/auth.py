899e838d0d63926d90a2a141f08993cf4c28fbcd
hukusi <hukushi.sakura@gmail.com>
2025-11-01 17:16:49 +0900

    user情報取得と新規登録の追加

--- routers/auth.py
+++ routers/auth.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from models import Users
+from passlib.context import CryptContext
 
 import models
 from database import engine, SessionLocal
 
 models.Base.metadata.create_all(bind=engine)
+
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
 def get_db():
@@ -13,6 +21,18 @@ def get_db():
         yield db
     finally:
         db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+    phone_number: str
         
 router = APIRouter(
     prefix='/auth',
@@ -23,3 +43,19 @@ router = APIRouter(
 def health_check():
     return {'status': 'auth'}
 
+@router.post("/")
+async def create_user(db: db_dependency,
+                      create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active=True,
+        phone_number=create_user_request.phone_number
+    )
+
+    db.add(create_user_model)
+    db.commit()
--- routers/users.py
+++ routers/users.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
 
 import models
 from database import engine, SessionLocal
+from models import Users
 
 models.Base.metadata.create_all(bind=engine)
+
 
 
 def get_db():
@@ -14,13 +18,23 @@ def get_db():
     finally:
         db.close()
         
+        
+db_dependency = Annotated[Session, Depends(get_db)]
+        
 router = APIRouter(
     prefix='/user',
     tags=['user']
 )
 
-@router.get("/")
-def health_check():
-    return {'status': 'user'}
+# @router.get("/")
+# def health_check():
+#     return {'status': 'user'}
 
+
+@router.get('/')
+async def get_user(username, db: db_dependency):
+    if username is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+
+    return db.query(Users).filter(Users.username == username).first()
 
