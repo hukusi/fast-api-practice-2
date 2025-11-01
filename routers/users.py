697fc4393fbee538735af72ca9d8854f67941733
hukusi <hukushi.sakura@gmail.com>
2025-11-01 18:18:34 +0900

    ユーザーidで取得

--- routers/users.py
+++ routers/users.py
@@ -43,3 +43,20 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+@router.get("/{user_id}", response_model=UserSchema)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(UserModel).filter(UserModel.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+# メモ追加
+# @router.post("/{user_id}/notes", response_model=Note)
+# def create_note_for_user(user_id: int, note: NoteCreate, db: Session = Depends(get_db)):
+#     db_note = Note(**note.dict(), user_id=user_id)
+#     db.add(db_note)
+#     db.commit()
+#     db.refresh(db_note)
+#     return db_note
